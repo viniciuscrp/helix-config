d64753264d97424a4c26b82bcaf78cede6d77ed5
viniciuscrp <vinicius.rodrigues04@outlook.com>
2026-06-30 20:11:02 -0300

    layout changes and added bootmagic

--- rev3_5rows/keymaps/vinicius/keymap.c
+++ rev3_5rows/keymaps/vinicius/keymap.c
@@ -29,11 +29,11 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    MO(_ADJUST),    KC_1,           KC_2,           KC_3,              KC_4,              KC_5,                                     KC_6,               KC_7,              KC_8,              KC_9,           KC_0,             KC_DEL,
-    KC_GRAVE,       KC_Q,           KC_W,           KC_E,              KC_R,              KC_T,                                     KC_Y,               KC_U,              KC_I,              KC_O,           KC_P,             KC_BSPC,
+    MO(_ADJUST),    KC_1,           KC_2,           KC_3,              KC_4,              KC_5,                                     KC_6,               KC_7,              KC_8,              KC_9,           KC_0,             KC_BSPC,
+    KC_GRAVE,       KC_Q,           KC_W,           KC_E,              KC_R,              KC_T,                                     KC_Y,               KC_U,              KC_I,              KC_O,           KC_P,             KC_DEL,
     KC_ESC,         LT(_SYM, KC_A), KC_S,           MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                     KC_H,               MT(MOD_LSFT, KC_J),MT(MOD_LCTL, KC_K),KC_L,           LT(_SYM, BR_SCLN),KC_ENTER,
     BR_BSLS,        KC_Z,           KC_X,           KC_C,              KC_V,              KC_B,           KC_MEH,                   KC_MEH,             KC_N,              KC_M,              KC_COMMA,       KC_DOT,           BR_SLSH,        KC_TRANSPARENT,
-    KC_CAPS,        KC_TRANSPARENT, KC_LEFT_GUI,    KC_NO,             MS_JIGGLER,        CW_TOGG,        LT(_FN, KC_SPACE),        LT(_NAV, KC_BSPC),  TT(_MOUSE),        KC_TRANSPARENT,    KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT, KC_TRANSPARENT
+    KC_CAPS,        KC_TRANSPARENT, KC_NO,          MS_JIGGLER,        KC_LEFT_GUI,       CW_TOGG,        LT(_FN, KC_SPACE),        LT(_NAV, KC_BSPC),  TT(_MOUSE),        KC_LEFT_ALT,       KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_ALT] = LAYOUT(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -71,18 +71,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_LALT,        KC_SPACE,       KC_M,                           KC_BSPC,        KC_ENTER,       KC_F5,          KC_F4,          KC_F3,          KC_F2,          KC_F1
   ),
   [_FN] = LAYOUT(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,              KC_NO,                                      KC_NO,          KC_NO,             KC_NO,             KC_NO,             KC_NO,                 KC_NO,
-    KC_NO,          KC_NO,          KC_F7,          KC_F8,          DUAL_FUNC_0,        DUAL_FUNC_1,                                KC_NO,          KC_7,              KC_8,              KC_9,              US_PLUS,               KC_NO,
-    KC_NO,          KC_NO,          KC_F4,          TD(DANCE_0),    KC_F6,              DUAL_FUNC_2,                                LSFT(KC_8),     MT(MOD_LSFT, KC_4),MT(MOD_LCTL, KC_5),MT(MOD_LALT, KC_6),MT(MOD_LGUI, KC_MINUS),KC_NO,
-    KC_NO,          KC_NO,          KC_F1,          KC_F2,          KC_F3,              DUAL_FUNC_3,    KC_NO,                      KC_NO,          KC_NO,             KC_1,              KC_2,              KC_3,                  KC_SLASH,       KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_SPACE,                   KC_BSPC,        KC_0,              KC_DOT,            KC_COMMA,          KC_NO,                 KC_NO,          KC_NO
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,             KC_NO,             KC_NO,             KC_NO,                 KC_NO,
+    KC_NO,          KC_NO,          KC_F7,          KC_F8,          DUAL_FUNC_0,    DUAL_FUNC_1,                                    KC_NO,          KC_7,              KC_8,              KC_9,              US_PLUS,               KC_NO,
+    KC_NO,          KC_NO,          KC_F4,          TD(DANCE_0),    KC_F6,          DUAL_FUNC_2,                                    LSFT(KC_8),     MT(MOD_LSFT, KC_4),MT(MOD_LCTL, KC_5),MT(MOD_LALT, KC_6),MT(MOD_LGUI, KC_MINUS),KC_NO,
+    KC_NO,          KC_NO,          KC_F1,          KC_F2,          KC_F3,          DUAL_FUNC_3,    KC_NO,                          KC_NO,          KC_NO,             KC_1,              KC_2,              KC_3,                  KC_SLASH,       KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_SPACE,                       KC_BSPC,        KC_0,              KC_DOT,            KC_COMMA,          KC_NO,                 KC_NO,          KC_NO
   ),
   [_MOUSE] = LAYOUT(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,           KC_NO,                                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_NO,          KC_NO,          LCTL(KC_W),     KC_NO,          LCTL(LSFT(KC_T)),LCTL(KC_T),                                    KC_NO,          LALT(KC_LEFT),  KC_MS_UP,       LALT(KC_RIGHT), KC_NO,          KC_NO,
     KC_ESC,         KC_MS_WH_LEFT,  KC_MS_BTN2,     KC_MS_BTN3,     KC_MS_BTN1,      KC_MS_WH_RIGHT,                                KC_NO,          KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_NO,          KC_TRANSPARENT,
     KC_NO,          KC_HOME,        KC_MS_WH_UP,    KC_NO,          KC_MS_WH_DOWN,   KC_END,         KC_NO,                         KC_NO,          KC_NO,          ST_MACRO_0,     KC_NO,          ST_MACRO_1,     KC_NO,          KC_NO,
-    KC_RIGHT_CTRL,  KC_NO,          KC_NO,          KC_NO,          KC_NO,           KC_NO,          KC_NO,                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRANSPARENT
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,           KC_NO,          KC_RIGHT_CTRL,                 KC_NO,          TT(_MOUSE),     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRANSPARENT
   ),
 };
 
@@ -202,7 +202,7 @@ void matrix_scan_user(void) {
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
-  if (last_input_activity_elapsed() > 30000) {
+  if (last_input_activity_elapsed() > 300000) {
     oled_off();
     return false;
   }
--- rev3_5rows/rules.mk
+++ rev3_5rows/rules.mk
@@ -1,8 +1,9 @@
+BOOTMAGIC_ENABLE = yes
 EXTRAKEY_ENABLE = yes    # Audio control and System control
 RGBLIGHT_ENABLE = yes   # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no
 OLED_ENABLE = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
 DIP_SWITCH_ENABLE = no
 MOUSEKEY_ENABLE = yes
 LTO_ENABLE = yes
