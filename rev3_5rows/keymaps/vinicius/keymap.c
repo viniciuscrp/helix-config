8beb13ec8fc97bdf66f7c32c8f77b05c094a1682
viniciuscrp <vinicius.rodrigues04@outlook.com>
2026-07-02 01:17:17 -0300

    changed cursor mode from toggle to momentary layer and moved mouse toggle to adjust layer

--- rev3_5rows/keymaps/vinicius/keymap.c
+++ rev3_5rows/keymaps/vinicius/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,       KC_Q,           KC_W,           KC_E,              KC_R,              KC_T,                                     KC_Y,               KC_U,              KC_I,              KC_O,           KC_P,             KC_DEL,
     KC_ESC,         LT(_SYM, KC_A), KC_S,           MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                     KC_H,               MT(MOD_LSFT, KC_J),MT(MOD_LCTL, KC_K),KC_L,           LT(_SYM, BR_SCLN),KC_ENTER,
     BR_BSLS,        KC_Z,           KC_X,           KC_C,              KC_V,              KC_B,           KC_MEH,                   KC_MEH,             KC_N,              KC_M,              KC_COMMA,       KC_DOT,           BR_SLSH,        KC_TRANSPARENT,
-    KC_CAPS,        KC_TRANSPARENT, KC_NO,          MS_JIGGLER,        KC_LEFT_GUI,       CW_TOGG,        LT(_FN, KC_SPACE),        LT(_NAV, KC_BSPC),  TT(_MOUSE),        KC_LEFT_ALT,       KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT, KC_TRANSPARENT
+    KC_CAPS,        KC_TRANSPARENT, KC_NO,          KC_NO,             KC_LEFT_GUI,       CW_TOGG,        LT(_FN, KC_SPACE),        LT(_NAV, KC_BSPC),  MO(_MOUSE),        KC_LEFT_ALT,       KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_ALT] = LAYOUT(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          TO(_BASE),      KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_BRIGHTNESS_DOWN, KC_AUDIO_VOL_UP,  KC_BRIGHTNESS_UP,   KC_PSCR,  KC_NO,
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          TO(_GAME),                                      KC_NO,          KC_MEDIA_PREV_TRACK,KC_AUDIO_VOL_DOWN,KC_MEDIA_NEXT_TRACK,KC_NO,    KC_NO,
     KC_NO,          KC_NO,          KC_NO,          TO(_ALT),       KC_NO,          KC_NO,          KC_NO,                          KC_NO,          KC_MEDIA_PLAY_PAUSE,KC_AUDIO_MUTE,    KC_NO,              KC_NO,    KC_NO,          KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                          KC_NO,          KC_NO,              KC_NO,            KC_NO,              KC_NO,    KC_NO,          KC_NO
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          MS_JIGGLER,                     KC_NO,          KC_NO,              KC_NO,            KC_NO,              KC_NO,    KC_NO,          KC_NO
   ),
   [_GAME] = LAYOUT(
     KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TO(_BASE),
